9009e6e497860811d8e5386a8a551a7610d3f94a
ehddn <ehddn2202@gmail.com>
2023-10-17 19:31:44 +0900

    10.17 과제

---
+++ django/10.17_homework/tube/tube/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import Post
+
+class CommentForm(forms.Form):
+    message = forms.CharField(widget=forms.Textarea)
+
+class PostForm(forms.ModelForm):
+  class Meta:
+    model = Post
+    fields = ['title',
+              'content',
+              'file_upload',
+              'author',
+              'tags']
